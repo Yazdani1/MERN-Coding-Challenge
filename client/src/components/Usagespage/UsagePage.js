2a127dcec8b23ae211044d0ed6538950d91b5a9a
Yazdani <yaz4dani@gmail.com>
2022-02-10 13:04:14 +0200

    error solved

--- client/src/components/Usagespage/UsagePage.js
+++ client/src/components/Usagespage/UsagePage.js
@@ -13,6 +13,14 @@ const UsagePage = () => {
 
   const [singlemachine, setSinglemachine] = useState([]);
 
+  //to calculatte contract price need to add usages state
+
+  const [usages, setUsages] = useState("");
+
+  //to show contract price
+
+  const [contractpriceresult, setContractpriceresult] = useState(0);
+
   //to load all the mashine name
   const loadallMachinelist = () => {
     getmachineContract()
@@ -44,6 +52,18 @@ const UsagePage = () => {
     loadallMachinelist();
   }, []);
 
+  //to get contract price
+
+  const contractprice = (e) => {
+    e.preventDefault();
+    var contractresult =
+      parseFloat(singlemachine.onetimefee) +
+      parseFloat(singlemachine.usagefee) * parseInt(usages);
+    console.log("Addition result:" + contractresult);
+    setContractpriceresult(contractresult);
+    // setUsages("");
+  };
+
   return (
     <React.Fragment>
       <div className="container card contract-price-cal">
@@ -67,6 +87,36 @@ const UsagePage = () => {
         <div className="container card machine-details">
           <div className="machine-details-item">
             <p>{singlemachine.machinename}</p>
+            <p>
+              One time fee: {parseFloat(singlemachine.onetimefee).toFixed(2)} +
+              Usage fee: {parseFloat(singlemachine.usagefee).toFixed(2)}
+            </p>
+            <form>
+              <div className="event-form">
+                <label for="exampleInputEmail1" className="form-label">
+                  Usages
+                </label>
+                <input
+                  type="number"
+                  value={usages}
+                  onChange={(e) => setUsages(e.target.value)}
+                  className="form-control"
+                />
+              </div>
+              <h6>
+                Contract Price: {parseFloat(contractpriceresult).toFixed(2)}
+              </h6>
+              <div class="form-group justify-content-center align-items-center">
+                <button
+                  type="submit"
+                  name="btnSubmit"
+                  className="create-event-button"
+                  onClick={contractprice}
+                >
+                  Contract Price
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       ) : (
